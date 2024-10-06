668a02357525b67c7b3403d529e46d762bc57cd0
Kuznetsov20 <andrusha02.2002@mail.ru>
2024-10-07 01:13:05 +0300

    Внесение поправок

--- src/main/java/com/trueman/attractions/controllers/AssistanceController.java
+++ src/main/java/com/trueman/attractions/controllers/AssistanceController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 /**
